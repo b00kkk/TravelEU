e8191cb2b3c1916f630115b32f1de8d4825d213a
qnrua0511 <qnrua0511@naver.com>
2024-10-29 14:47:18 +0900

    로그인시 시작가능

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,11 +4,13 @@
   import UserCreate from './routes/UserCreate.svelte'
   import UserLogin from './routes/UserLogin.svelte'
   import Navigation from "./components/Navigation.svelte"
+  import DateSelect from './routes/DateSelect.svelte'
 
   const routes = {
     '/': Home,
     '/user-create': UserCreate,
     '/user-login': UserLogin,
+    '/date-select':DateSelect
   }
 </script>
 
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,1 +1,41 @@
-<h1>Hello</h1>
+<script>
+    import { is_login } from "../lib/store";
+    import { push } from 'svelte-spa-router';
+    import UserLogin from './UserLogin.svelte';
+  
+    // 로그인 여부를 확인하는 함수
+    function handleButtonClick() {
+      if ($is_login) {
+        push("/date-select");  // 로그인 시 DateSelect 페이지로 이동
+      } else {
+        alert("로그인을 해주세요."); // 로그인 팝업
+      }
+    }
+  </script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* 전체 화면 중앙 정렬 */
+  }
+  
+  .button {
+    padding: 40px 80px;
+    font-size: 40px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .button:hover {
+    background-color: #2980b9;
+  }
+</style>
+
+<div class="container">
+  <button class="button" on:click={handleButtonClick}>Start</button>
+</div>
